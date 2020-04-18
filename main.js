c126ef09a56b05cae902c9c0d086f67fd6692107
CoxSpen <coxspen@oregonstate.edu>
2020-04-18 00:24:52 -0500

    Got Node running on OSU server. Still have to work on routing of html pages.

---
+++ main.js
@@ -0,0 +1,40 @@
+/*********************************************************************************
+ ** Program Name:	main.js
+ ** Authors:		Team Band Aid
+ ** Date:		    4/17/2020
+ ** Description:	js to run Band Aid Home Page
+ *******************************************************************************/
+
+var express = require('express');
+//var mysql = require('./dbcon.js');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.engine('handlebars', handlebars.engine);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+//app.set('mysql', mysql);
+
+app.use('/home', require('./home.js'));
+app.use('/', express.static('public'));
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
