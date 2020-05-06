11df13519ddd1e88dd618342bb5f55b64386407c
johnsna7 <50123256+johnsna7@users.noreply.github.com>
2020-05-05 21:57:10 -0500

    Query Update
    Added sample data & updated spelling

--- bandaid_database_definitions_042220.sql
+++ bandaid_database_definitions_042220.sql
@@ -19,7 +19,7 @@ CREATE TABLE Users(
 CREATE TABLE Queries(
     query_id INT AUTO_INCREMENT,
     radius INT NOT NULL,
-    instrument VARCHAR(1) NOT NULL,
+    instrument VARCHAR(20) NOT NULL,
     proficiency INT NOT NULL,
     description VARCHAR(500),
     PRIMARY KEY(query_id)
@@ -116,3 +116,143 @@ ADD CONSTRAINT fk_musicians_bands_2
 FOREIGN KEY (musician_id)
 REFERENCES Musicians(musician_id)
 ON DELETE CASCADE;
+
+
+------------------------------------------------------------------
+-- SAMPLE DATA
+------------------------------------------------------------------
+
+INSERT INTO Users (email, password, fname, lname, phone, zip)
+VALUES ('user1@gmail.com', 'password', 'Bob', 'Jones', '123456789','60523');
+
+INSERT INTO Users (email, password, fname, lname, phone, zip)
+VALUES ('user2@gmail.com', 'password2', 'Dave', 'Jones', '123456789','99999');
+
+INSERT INTO Users (email, password, fname, lname, phone, zip)
+VALUES ('user3@gmail.com', 'password3', 'Betty', 'Bop', '123456789','11111');
+
+INSERT INTO Users (email, password, fname, lname, phone, zip)
+VALUES ('user4@gmail.com', 'password4', 'Mike', 'Mustache', '123456789','60523');
+
+INSERT INTO Users (email, password, fname, lname, phone, zip)
+VALUES ('user5@gmail.com', 'password5', 'Sally', 'Sandal', '123456789','99999');
+
+
+
+INSERT INTO Queries (radius, instrument, proficiency, description)
+VALUES ('5', 'Guitar', '5', 'Rock gig');
+
+INSERT INTO Queries (radius, instrument, proficiency, description)
+VALUES ('20', 'Piano', '9', 'Looking for someone to accompany a vocalist');
+
+INSERT INTO Queries (radius, instrument, proficiency, description)
+VALUES ('15', 'Drums', '7', 'Play with our band in some clubs');
+
+INSERT INTO Queries (radius, instrument, proficiency, description)
+VALUES ('530', 'Voice', '10', 'Looking to add a member for our tour');
+
+INSERT INTO Queries (radius, instrument, proficiency, description)
+VALUES ('20', 'Bass', '3', 'Lets jam out man');
+
+
+
+INSERT INTO Instruments (name, proficiency)
+VALUES ('guitar', '7');
+
+INSERT INTO Instruments (name, proficiency)
+VALUES ('voice', '6');
+
+INSERT INTO Instruments (name, proficiency)
+VALUES ('drums', '2');
+
+INSERT INTO Instruments (name, proficiency)
+VALUES ('piano', '5');
+
+INSERT INTO Instruments (name, proficiency)
+VALUES ('keytar', '9');
+
+
+
+INSERT INTO Musicians (lfg, demo_link, instrument_id)
+VALUES ('1', 'soundcloud.com', '1');
+
+INSERT INTO Musicians (lfg, demo_link, instrument_id)
+VALUES ('1', 'soundcloud.com', '2');
+
+INSERT INTO Musicians (lfg, demo_link, instrument_id)
+VALUES ('0', 'soundcloud.com', '3');
+
+INSERT INTO Musicians (lfg, demo_link, instrument_id)
+VALUES ('1', 'soundcloud.com', '4');
+
+INSERT INTO Musicians (lfg, demo_link, instrument_id)
+VALUES ('1', 'soundcloud.com', '5');
+
+
+
+INSERT INTO Bands (name, bio, demo_reel)
+VALUES ('The Bugs', 'Our music will bug you', 'soundcloud.com');
+
+INSERT INTO Bands (name, bio, demo_reel)
+VALUES ('Wind', 'Our music will blow you away', 'soundcloud.com');
+
+INSERT INTO Bands (name, bio, demo_reel)
+VALUES ('The Heavywaits', 'Our music is weigh cool', 'soundcloud.com');
+
+INSERT INTO Bands (name, bio, demo_reel)
+VALUES ('Monolith', 'Our music will rock you', 'soundcloud.com');
+
+INSERT INTO Bands (name, bio, demo_reel)
+VALUES ('The Stroodles', 'Our music is oodles of fun', 'soundcloud.com');
+
+
+
+INSERT INTO Musicians_Bands (band_id, musician_id)
+VALUES ('3', '2');
+
+INSERT INTO Musicians_Bands (band_id, musician_id)
+VALUES ('2', '2');
+
+INSERT INTO Musicians_Bands (band_id, musician_id)
+VALUES ('3', '3');
+
+INSERT INTO Musicians_Bands (band_id, musician_id)
+VALUES ('2', '4');
+
+INSERT INTO Musicians_Bands (band_id, musician_id)
+VALUES ('3', '5');
+
+
+
+
+INSERT INTO Messages (header, body, response)
+VALUES ('Looking for guitarists', 'Must be okayish', '0');
+
+INSERT INTO Messages (header, body, response)
+VALUES ('Looking for vocalists', 'Payment through exposure', '1');
+
+INSERT INTO Messages (header, body, response)
+VALUES ('Looking for pianists', 'Please teach me for free', '0');
+
+INSERT INTO Messages (header, body, response)
+VALUES ('Looking for drummers', 'Eating chicken doesnt count', '0');
+
+INSERT INTO Messages (header, body, response)
+VALUES ('Looking for keytarists', 'Do you exist?', '0');
+
+
+
+INSERT INTO Users_Messages (user_id, msg_id)
+VALUES ('1', '1');
+
+INSERT INTO Users_Messages (user_id, msg_id)
+VALUES ('2', '1');
+
+INSERT INTO Users_Messages (user_id, msg_id)
+VALUES ('3', '3');
+
+INSERT INTO Users_Messages (user_id, msg_id)
+VALUES ('4', '5');
+
+INSERT INTO Users_Messages (user_id, msg_id)
+VALUES ('4', '2');
--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host : 'band-aid-madiadb.cdcjmss5gofv.us-east-1.rds.amazonaws.com',
-  user : 'admin',
-  password : 'v3rys3cur3',
-  database : 'band-aid-madiadb',
+  host : '127.0.0.1',
+  user : 'root',
+  password : '',
+  database : 'sys',
 });
 
 module.exports.pool = pool;
