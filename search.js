265976a1e34b27019802095e7fa1371a316c6d33
Chopsaru <45904825+Chopsaru@users.noreply.github.com>
2020-05-10 15:41:54 -0400

    forgot to save before last commit.. see previous description

--- search.js
+++ search.js
@@ -2,39 +2,46 @@ module.exports = function(){
     let express = require('express');
     let router = express.Router();
 
-    function getUserQuery(req, mysql){
-        // Construct query--------------------------------------------------------------
-        let sql = "SELECT * FROM Queries where query_id = ?";
-        inserts = [req.params.uid];
-
-        // Query and store results------------------------------------------------------
-        console.log("SELECT * FROM Queries where query_id = " + req.params.uid)
-        mysql.pool.query(sql, inserts, function(error, results){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-               }
-               return results[0]
-           });
-   }
-
     function getMatchingMusicians(res, req, mysql, context, complete){
+        //now entering callback hell
+
         //get query rows with matching userid
-        var queryResults = getUserQuery(req, mysql)
+            // Construct query--------------------------------------------------------------
+            let sql = "SELECT * FROM Queries where query_id = ?";
+            inserts = [req.params.uid];
+
+            // Query and store results------------------------------------------------------
+            console.log("SELECT * FROM Queries where query_id = " + req.params.uid)
+            mysql.pool.query(sql, inserts, function(error, queryResults){
+                if(error){
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }
+                else{
+                    console.log(queryResults)
+                    var uid = req.params.uid;
+                    var qid = queryResults[0].query_id;
+                    var qzip = queryResults[0].zip_code;
+                    var qinstrument = queryResults[0].instrument;
+                    var qproficiency = queryResults[0].proficiency;
+                    var qdescription = queryResults[0].description;
+
+                    //get user zip code from database
+
+                    //get musicians with the same first 2 zip code numbers
+                    //eventually, a radius alrogithm will replace this
+
+                    //get musician ids from instrument table that match with user querys
+
+                    //get musician/user information 
+
+                    //reformat data into json or store to context
+                }
+            });
+
         
-        console.log(queryResults)
 
-        //get user zip code from database
-
-        //get s musicians with the same first 2 zip code numbers
-        //eventually, a radius alrogithm will replace this
-
-        //get musician ids from instrument table that match with user querys
-
-        //get musician/user information 
-
-        //reformat data into json or store to context
-
+        //now leaving callback hell
         complete();
     }
 
