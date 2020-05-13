93bcc474af5fc2d03e3b8192b91bc9bed3706099
Chopsaru <45904825+Chopsaru@users.noreply.github.com>
2020-05-13 18:50:28 -0400

    updated dbcon with new server host name

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host : 'band-aid-madiadb.cdcjmss5gofv.us-east-1.rds.amazonaws.com',
+  host : 'mysqlbandaid.cfrmcgrcrjey.us-east-1.rds.amazonaws.com',
   user : 'admin',
   password : 'v3rys3cur3',
   database : 'Band_Aid_mariadb',
