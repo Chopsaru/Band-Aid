4d3d5edd6acff6d4f11b9bbb1874eb0ac14c5d56
CoxSpen <coxspen@oregonstate.edu>
2020-05-23 21:50:09 -0500

    Revert "Fixed full user edit update"
    This reverts commit 704d35ec8f63325b1747b0ba2296d8b699c8489f.
    
    # Conflicts:
    #	login.js
    #	main.js
    #	user_profile.js
    #	views/user_profile.handlebars

--- main.js
+++ main.js
@@ -1,82 +1,74 @@
-const express = require('express')
+const express = require('express');
 const session = require('express-session')
-const mysql = require('./dbcon.js')
-const app = express()
-const NodeCache = require("node-cache");
-const in_mem_cache = new NodeCache();
-
-const handlebars = require('express-handlebars').create({
-  helpers: require("./helpers.js").helpers,
-  defaultLayout: 'main'
-})
-const bodyParser = require('body-parser')
-const ip = require('ip')
+const mysql = require('./dbcon.js');
+const app = express();
+const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+const bodyParser = require('body-parser');
+const ip = require('ip');
 
 // cookie lifetime
 const TWO_HOURS = 1000 * 60 * 60 * 2
 
 // env variables
-const {
-  NODE_ENV = 'development',
-  SESSION_NAME = 'sid',
-  SESSION_SECRET = 'super-secret-code',
-  SESSION_LIFETIME = TWO_HOURS
-} = process.env
+const{
+    NODE_ENV = 'development',
+    SESSION_NAME = 'sid',
+    SESSION_SECRET = 'super-secret-code',
+    SESSION_LIFETIME = TWO_HOURS
+}= process.env
 const IN_PROD = NODE_ENV === 'production'
 
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', 'handlebars')
-app.set('port', 5000)
-app.set('mysql', mysql)
-app.set('in_mem_cache', in_mem_cache);
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5000);
+app.set('mysql', mysql);
 
 // expression-session configuration
 app.use(session({
-  name: SESSION_NAME,
-  resave: false,
-  saveUninitialized: false,
-  secret: SESSION_SECRET,
-  cookie: {
-    maxAge: SESSION_LIFETIME,
-    sameSite: true,
-    secure: IN_PROD,                // look into mysql session store
-  }
+    name: SESSION_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: SESSION_LIFETIME,
+        sameSite: true,
+        secure: IN_PROD,                // look into mysql session store
+    }
 }))
 
 // body parser
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 // point at public directory folder
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
 
 // pages in site
-app.use('/', require('./home.js'))
-app.use('/login', require('./login.js'))
-app.use('/logout', require('./logout.js'))
-app.use('/bad_login', require('./bad_login.js'))
-app.use('/sign_up', require('./sign_up.js'))
-app.use('/user_profile', require('./user_profile.js'))
-app.use('/search', require('./search.js'))
-app.use('/search/q1', require('./q1.js'))
-app.use('/search/q2', require('./q2.js'))
-app.use('/search/q3', require('./q3.js'))
+app.use('/', require('./home.js'));
+app.use('/login', require('./login.js'));
+app.use('/sign_up', require('./sign_up.js'));
+app.use('/user_profile', require('./user_profile.js'));
+app.use('/search', require('./search.js'));
+app.use('/search/q1', require('./q1.js'));
+app.use('/search/q2', require('./q2.js'));
+app.use('/search/q3', require('./q3.js'));
 
 // error handling
-app.use(function (req, res) {
-  res.status(404)
-  res.render('404')
-})
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.type('plain/text')
-  res.status(500)
-  res.render('500')
-})
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
 
 // node start up
-app.listen(app.get('port'), function () {
-  console.log(ip.address())
-  console.log(`Express started on http://${ip.address()}:5000/` + '; press Ctrl-C to terminate.')
-})
+app.listen(app.get('port'), function(){
+    console.log(ip.address());
+    console.log(`Express started on http://${ip.address()}:5000/` +  '; press Ctrl-C to terminate.');
+});
--- user_profile.js
+++ user_profile.js
@@ -1,232 +1,179 @@
-// -------------------------------------------user profile landing page------------------------------------------
-module.exports = (function () {
-    let express = require('express')
-    let formidable = require('formidable')
-    let fs = require('fs')
-    let router = express.Router()
 
-    // ------------------------------------------ session handlers --------------------------------------------
+//-------------------------------------------user profile landing page--------------------------------------------------
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+//----------------------------------------------- session handlers -----------------------------------------------------
     // handles user if not signed in
-    const redirectLogin = (req, res, next) => {
-      if (!req.session.userId) {
-        res.redirect('/login')
-      } else {
-        next()
-      }
-    }
-
-    // handles user if signed in
-    const redirectUser_Profile = (req, res, next) => {
-      if (req.session.userId) {
-        res.redirect('/user_profile/' + req.session)
-      } else {
-        next()
-      }
-    }
-
-    // --------------------------------- get single user profile data -----------------------------------------
-    function getUserProfile (res, mysql, context, id, complete) {
-      // Construct query--------------------------------------------------------------
-      var sql = 'SELECT user_id as id, fname, lname, name as insName, level as insProficiency, email, password, phone, social, zip, lfg, demo_link, profile_image\n' +
-        'FROM users\n' +
-        'INNER JOIN instruments\n' +
-        'ON users.instrument_id = instruments.instrument_id\n' +
-        'INNER JOIN proficiencies \n' +
-        'ON users.proficiency_id = proficiencies.proficiency_id WHERE user_id = ?'
-      var inserts = [id]
-      // Query and store results------------------------------------------------------
-      mysql.pool.query(sql, inserts, function (error, results) {
-        if (error) {
-          res.write(JSON.stringify(error))
-          res.end()
-        }
-        context.user_profile = results[0]
-        context.uid = results[0].id
-        complete()
-      })
-    }
-
-    // --------------------------------------- get single user profile data -------------------------------------------------
-
-    function getInstruments (res, mysql, context, complete) {
-      mysql.pool.query('SELECT instrument_id as id, name as insName FROM instruments', function (error, results) {
-        if (error) {
-          res.write(JSON.stringify(error))
-          res.end()
-        }
-        context.instruments = results
-        console.log(results)
-        complete()
-      })
-    }
-
-    // --------------------------------------- get single user profile data -------------------------------------------------
-
-    function getProficiency (res, mysql, context, complete) {
-      // Construct query--------------------------------------------------------------
-      var sql = 'SELECT proficiency_id as id, level as insProficiency FROM proficiencies'
-      // Query and store results------------------------------------------------------
-      mysql.pool.query(sql, function (error, results) {
-        if (error) {
-          res.write(JSON.stringify(error))
-          res.end()
-        }
-        context.proficiencies = results
-        complete()
-      })
-    }
-
-    // ------------------------------------------ get and display single user -----------------------------------------------
-
-    router.get('/:id', redirectLogin, function (req, res) {
-      console.log(req.session)
-      console.log(req.session.userId)
-
-      var callbackCount = 0
-      var context = {}
-      context.jsscripts = ['edit_user_profile.js', 'delete_user_profile.js']
-      var mysql = req.app.get('mysql')
-
-      // get all data for update
-      getUserProfile(res, mysql, context, req.session.userId, function () {
-        if (context.user_profile.profile_image === null) {
-          context.user_profile.profile_image = '/images/no_profile.png'
+    const redirectLogin = (req, res, next) =>{
+        if(!req.session.userId){
+            res.redirect('/login')
         } else {
-          let cache = req.app.get('in_mem_cache');
-          const imgdata = cache.get(context.user_profile.profile_image);
-          if(imgdata === undefined) {
-            context.user_profile.profile_image = '/images/no_profile.png'
-          } else {
-            context.user_profile.profile_image = "data:image/gif;base64," + cache.get(context.user_profile.profile_image);
-          }
+            next()
         }
-        complete();
-      })
-
-      console.log('Made it back to redirect')
-
-      function complete () {
-        callbackCount++
-        if (callbackCount >= 1) {
-          if (callbackCount >= 1) {
-            res.render('user_profile', context)
-          }
-        }
-      }
-
-    })
-
-    // ----------------------------------- get and display single user for editing ------------------------------------------
-
-    router.get('/edit/:id', redirectLogin, function (req, res) {
-      var callbackCount = 0
-      var context = {}
-      context.jsscripts = ['edit_user_profile.js', 'delete_user_profile.js']
-      var mysql = req.app.get('mysql')
-
-      getUserProfile(res, mysql, context, req.params.id, complete)
-      getInstruments(res, mysql, context, complete)
-      getProficiency(res, mysql, context, complete)
-
-      function complete () {
-        callbackCount++
-        if (callbackCount >= 3) {
-          res.render('edit_user_profile', context)
-        }
-      }
-    })
-
-    // ----------------------------------- change password page ####not functioning yet######--------------------------------
-
-    router.get('/edit/password/:id', redirectLogin, function (req, res) {
-      var callbackCount = 0
-      var context = {}
-      context.jsscripts = ['edit_user_profile.js', 'delete_user_profile.js']
-      var mysql = req.app.get('mysql')
-
-      getUserProfile(res, mysql, context, req.params.id, complete)
-
-      function complete () {
-        callbackCount++
-        if (callbackCount >= 1) {
-          res.render('update_password', context)
-        }
-      }
-    })
-
-    // ----------------------------------- updates database for user_profile change -----------------------------------------
-    // Need to get instrument, skill level, and gig going #############################################
-
-    router.put('/:id', function (req, res) {
-      console.log(req.body)
-      console.log(req.params.id)
-      var mysql = req.app.get('mysql') // need to add looking for gig
-      mysql.pool.query('UPDATE Users SET fname=?, lname=?, email=?, phone=?, zip=?, instrument_id=?, proficiency_id=?, social=?, demo_link=?  WHERE user_id=?',
-        [req.body.fname, req.body.lname, req.body.email, req.body.phone, req.body.zip, req.body.instrument_id, req.body.proficiency_id, req.body.social, req.body.demo_link, req.params.id],
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.write(JSON.stringify(error))
-            res.end()
-          } else {
-            res.status(200)
-            res.end()
-          }
-        })
-    })
-
-    // --------------------------------------------- delete user profiles ---------------------------------------------------
-    router.delete('/:id', function (req, res) {
-      var mysql = req.app.get('mysql')
-      mysql.pool.query('DELETE FROM Users WHERE user_id = ?', req.params.id, function (error) {
-        if (error) {
-          res.write(JSON.stringify(error))
-          res.status(400)
-          res.end()
-        } else {
-          res.status(202).end()
-          req.session.destroy()
-        }
-      })
-    })
-
-    function base64_encode(file) {
-      // read binary data
-      var bitmap = fs.readFileSync(file);
-      // convert binary data to base64 encoded string
-      return new Buffer(bitmap).toString('base64');
     }
 
-    router.post('/edit/profile_image/:id', function (req, res) {
-      console.log('Editing profile image:' + req)
-      let form = new formidable.IncomingForm()
-      let uid = req.params.id
-      let cache = req.app.get('in_mem_cache')
+// handles user if signed in
+    const redirectUser_Profile = (req, res, next) =>{
+        if(req.session.userId){
+            res.redirect('/user_profile/' + req.session)
+        } else {
+            next()
+        }
+    }
 
-      form.parse(req, function (err, fields, files) {
-        console.log(files, fields)
-        let oldpath = files.image.path
-        let img_ext = files.image.name.split('.')[1]
-        let newpath = `/profile_images/${uid}.${img_ext}`
-        console.log(`Saving image file from ${oldpath} to ${newpath}`)
-        let img = base64_encode(oldpath);
-        cache.set(newpath, img)
-        var mysql = req.app.get('mysql') // need to add looking for gig
-        mysql.pool.query('UPDATE Users SET profile_image=? WHERE user_id=?',
-          [newpath, uid],
-          function (error) {
-            if (error) {
-              console.log(error)
-              res.write(JSON.stringify(error))
-              res.end()
-            } else {
-              res.redirect(`/user_profile/${uid}`)
+//--------------------------------------- get single user profile data -------------------------------------------------
+
+    function getUserProfile(res, mysql, context, id, complete){
+
+        // Construct query--------------------------------------------------------------
+        var sql = "SELECT user_id as id, fname, lname, name as insName, level as insProficiency, email, password, phone, social, zip, lfg, demo_link\n" +
+            "FROM users\n" +
+            "INNER JOIN instruments\n" +
+            "ON users.instrument_id = instruments.instrument_id\n" +
+            "INNER JOIN proficiencies \n" +
+            "ON users.proficiency_id = proficiencies.proficiency_id WHERE user_id = ?";
+        var inserts = [id];
+        // Query and store results------------------------------------------------------
+        mysql.pool.query(sql, inserts, function(error, results){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
             }
-          })
-      })
-    })
+            context.user_profile = results[0];
+            complete();
+        });
+    }
 
-    return router
-  }
-  ()
-)
+//--------------------------------------- get single user profile data -------------------------------------------------
+
+    function getInstruments(res, mysql, context, complete){
+
+            mysql.pool.query("SELECT instrument_id as id, name as insName FROM instruments",function(error, results){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.instruments = results;
+            console.log(results);
+            complete();
+        });
+    }
+
+//--------------------------------------- get single user profile data -------------------------------------------------
+
+    function getProficiency(res, mysql, context, complete){
+
+        // Construct query--------------------------------------------------------------
+        var sql = "SELECT proficiency_id as id, level as insProficiency FROM proficiencies";
+        // Query and store results------------------------------------------------------
+        mysql.pool.query(sql, function(error, results){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.proficiencies = results;
+            complete();
+        });
+    }
+
+//------------------------------------------ get and display single user -----------------------------------------------
+
+    router.get('/:id', redirectLogin, function(req,res) {
+        console.log(req.session);
+        console.log(req.session.userId);
+
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["edit_user_profile.js","delete_user_profile.js"];
+        var mysql = req.app.get('mysql');
+
+        // get all data for update
+        getUserProfile(res, mysql, context, req.session.userId, complete);
+
+        console.log("Made it back to redirect")
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('user_profile', context);
+            }
+        }
+    });
+
+//----------------------------------- get and display single user for editing ------------------------------------------
+
+    router.get('/edit/:id',redirectLogin, function(req,res) {
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["edit_user_profile.js","delete_user_profile.js"];
+        var mysql = req.app.get('mysql');
+
+        getUserProfile(res, mysql, context, req.params.id, complete);
+        getInstruments(res, mysql, context, complete);
+        getProficiency(res, mysql, context, complete);
+
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 3){
+                res.render('edit_user_profile', context);
+            }
+        }
+    });
+
+//----------------------------------- change password page ####not functioning yet######--------------------------------
+
+    router.get('/edit/password/:id', redirectLogin, function(req,res) {
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["edit_user_profile.js","delete_user_profile.js"];
+        var mysql = req.app.get('mysql');
+
+        getUserProfile(res, mysql, context, req.params.id, complete);
+
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('update_password', context);
+            }
+        }
+    });
+
+//----------------------------------- updates database for user_profile change -----------------------------------------
+    //Need to get instrument, skill level, and gig going #############################################
+
+        router.put('/:id', function(req, res){
+            console.log(req.body);
+            console.log(req.params.id);
+            var mysql = req.app.get('mysql');
+            mysql.pool.query("UPDATE Users SET email=?, fname=?, lname=?, phone=?, zip=? WHERE user_id=?",
+                [req.body.email, req.body.fname, req.body.lname, req.body.phone, req.body.zip, req.params.id],
+                function(error){
+                    if(error){
+                        console.log(error);
+                        res.write(JSON.stringify(error));
+                        res.end();
+                    }else{
+                        res.status(200);
+                        res.end();
+                    }
+                })
+        ;});
+
+//--------------------------------------------- delete user profiles ---------------------------------------------------
+        router.delete('/:id', function(req, res){
+            var mysql = req.app.get('mysql');
+            mysql.pool.query("DELETE FROM Users WHERE user_id = ?", req.params.id, function(error){
+                if(error){
+                    res.write(JSON.stringify(error));
+                    res.status(400);
+                    res.end();
+                }else{
+                    res.status(202).end();
+                    req.session.destroy();
+                }
+            })
+        });
+
+    return router;
+}();
